c72390322edaa8614cba6ed211539bc9d20673e4
Danijel Lapadatovic <l.danijel977@gmail.com>
2022-12-28 18:12:55 +0100

    Finished widgets and Home page

--- admin-dashboard/src/components/widgetLarge/WidgetLarge.js
+++ admin-dashboard/src/components/widgetLarge/WidgetLarge.js
@@ -1,9 +1,53 @@
 import React from 'react'
 import './widgetLarge.css'
 export default function WidgetLarge() {
+  const Button = ({type}) => {
+    return <button className={'widget-large-button '+ type}>{type}</button>
+  }
   return (
     <div className='widget-large-container'>
-      
+      <h3 className="widget-large-title">Latest transactions</h3>
+      <table className="widget-large-table">
+        <tr className="widget-large-tr">
+          <th className="widget-large-th">Customer</th>
+          <th className="widget-large-th">Date</th>
+          <th className="widget-large-th">Amount</th>
+          <th className="widget-large-th">Status</th>
+        </tr>
+        <tr className="widget-large-tr">
+          <td className="widget-large-user">
+            <img src="/assets/person/8.jpeg" alt="" className="widget-large-img" />
+            <span className="widget-large-name">Susan Carol</span>
+          </td>
+          <td className="widget-large-date">2 Jun 2021</td>
+          <td className="widget-large-amount">$122.00</td>
+          <td className="widget-large-status">
+            <Button type="Aproved" />
+          </td>
+        </tr>
+        <tr className="widget-large-tr">
+          <td className="widget-large-user">
+            <img src="/assets/person/8.jpeg" alt="" className="widget-large-img" />
+            <span className="widget-large-name">Susan Carol</span>
+          </td>
+          <td className="widget-large-date">2 Jun 2021</td>
+          <td className="widget-large-amount">$122.00</td>
+          <td className="widget-large-status">
+            <Button type="Declined" />
+          </td>
+        </tr>
+        <tr className="widget-large-tr">
+          <td className="widget-large-user">
+            <img src="/assets/person/8.jpeg" alt="" className="widget-large-img" />
+            <span className="widget-large-name">Susan Carol</span>
+          </td>
+          <td className="widget-large-date">2 Jun 2021</td>
+          <td className="widget-large-amount">$122.00</td>
+          <td className="widget-large-status">
+            <Button type="Pending" />
+          </td>
+        </tr>
+      </table>
     </div>
   )
 }
--- admin-dashboard/src/components/widgetSmall/WidgetSmall.js
+++ admin-dashboard/src/components/widgetSmall/WidgetSmall.js
@@ -6,15 +6,46 @@ export default function WidgetSmall() {
     <div className='widget-small-container'>
         <span className="widget-small-title"> New Join Member</span>
         <ul className="widget-small-list">
-            <li className="widget-list-item">
+            <li className="widget-small-list-item">
                 <img className="widget-small-img" src="/assets/person/1.jpeg" alt="" />
                 <div className="widget-small-user">
                     <span className="widget-small-username">Anna Keler</span>
-                    <span className="widget-small-title">Software Engineer</span>
-                    <button className="small-button"> <MdOutlineVisibility/> Display</button>
+                    <span className="widget-small-position">Software Engineer</span>
                </div>
+               <button className="widget-small-button"> <MdOutlineVisibility className='widget-small-visibility-icon'/> Display</button>
             </li>
-            
+            <li className="widget-small-list-item">
+                <img className="widget-small-img" src="/assets/person/1.jpeg" alt="" />
+                <div className="widget-small-user">
+                    <span className="widget-small-username">Anna Keler</span>
+                    <span className="widget-small-position">Software Engineer</span>
+               </div>
+               <button className="widget-small-button"> <MdOutlineVisibility className='widget-small-visibility-icon'/> Display</button>
+            </li>
+            <li className="widget-small-list-item">
+                <img className="widget-small-img" src="/assets/person/1.jpeg" alt="" />
+                <div className="widget-small-user">
+                    <span className="widget-small-username">Anna Keler</span>
+                    <span className="widget-small-position">Software Engineer</span>
+               </div>
+               <button className="widget-small-button"> <MdOutlineVisibility className='widget-small-visibility-icon'/> Display</button>
+            </li>
+            <li className="widget-small-list-item">
+                <img className="widget-small-img" src="/assets/person/1.jpeg" alt="" />
+                <div className="widget-small-user">
+                    <span className="widget-small-username">Anna Keler</span>
+                    <span className="widget-small-position">Software Engineer</span>
+               </div>
+               <button className="widget-small-button"> <MdOutlineVisibility className='widget-small-visibility-icon'/> Display</button>
+            </li>
+            <li className="widget-small-list-item">
+                <img className="widget-small-img" src="/assets/person/1.jpeg" alt="" />
+                <div className="widget-small-user">
+                    <span className="widget-small-username">Anna Keler</span>
+                    <span className="widget-small-position">Software Engineer</span>
+               </div>
+               <button className="widget-small-button"> <MdOutlineVisibility className='widget-small-visibility-icon'/> Display</button>
+            </li>
         </ul>
     </div>
   )
